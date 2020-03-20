6bf650a95865167e0019f88dd39a09e2857a0898
Ken Collier <kencollier@MacBookPro.local>
2020-03-20 11:40:47 -0600

    Fixes bottom icons and text wrapping

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -23,7 +23,7 @@ export default function BottomTabNavigator({ navigation, route }) {
         component={Level1Screen}
         options={{
           title: 'Level 1',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-menu" />,
         }}
       />
       <BottomTab.Screen
@@ -31,7 +31,7 @@ export default function BottomTabNavigator({ navigation, route }) {
         component={Level2Screen}
         options={{
           title: 'Level 2',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-menu" />,
         }}
       />
       <BottomTab.Screen
@@ -39,7 +39,7 @@ export default function BottomTabNavigator({ navigation, route }) {
         component={Level3Screen}
         options={{
           title: 'Level 3',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-menu" />,
         }}
       />
       <BottomTab.Screen
@@ -47,7 +47,7 @@ export default function BottomTabNavigator({ navigation, route }) {
         component={Level4Screen}
         options={{
           title: 'Level 4',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-menu" />,
         }}
       />
       <BottomTab.Screen
@@ -55,7 +55,7 @@ export default function BottomTabNavigator({ navigation, route }) {
         component={Level5Screen}
         options={{
           title: 'Level 5',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-menu" />,
         }}
       />
     </BottomTab.Navigator>
--- screens/Level1Screen.js
+++ screens/Level1Screen.js
@@ -1,13 +1,12 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import * as WebBrowser from 'expo-web-browser';
 import { RectButton,  FlatList } from 'react-native-gesture-handler';
 import level1 from '../data/level1.json';
 
 export default function Level1Screen() {
-  const [color, setColor] = useState('white');
-  const [textColor, setTextColor] = useState('pink');
+  const [color, setColor] = useState(styles.option.backgroundColor);
+  const [textColor, setTextColor] = useState(styles.optionText);
 
 return (
       <FlatList
@@ -20,9 +19,10 @@ return (
               <OptionButton
                       icon="ios-chatboxes"
                       label={`${index + 1}. ${item.question}`}
+                      onPress={console.log(color)}
                       isLastOption
               />
-              );
+              );     
             }}
             keyExtractor={(item, index) => index.toString()}
       >
@@ -32,10 +32,15 @@ return (
 
 function OptionButton({ icon, label, onPress, isLastOption }) {
   return (
-    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
+    <RectButton 
+      style={[styles.option, isLastOption && styles.lastOption]} 
+      onPress={onPress}>
       <View style={{ flexDirection: 'row' }}>
         <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
+          <Ionicons 
+            name={icon} 
+            size={22} 
+            color="rgba(0,0,0,0.35)" />
         </View>
         <View style={styles.optionTextContainer}>
           <Text style={styles.optionText}>{label}</Text>
@@ -54,7 +59,7 @@ const styles = StyleSheet.create({
     paddingTop: 15,
   },
   optionIconContainer: {
-    marginRight: 12,
+    marginRight: 10,
   },
   option: {
     backgroundColor: 'pink',
@@ -68,9 +73,10 @@ const styles = StyleSheet.create({
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
   optionText: {
-    fontSize: 15,
+    fontSize: 16,
     color: 'purple',
-    alignSelf: 'flex-start',
+    alignSelf: 'auto',
     marginTop: 1,
+    marginRight: 20
   },
 });
